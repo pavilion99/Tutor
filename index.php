9719c244d1356c5fcd0e2454cf5b8eda168b8a4f
Spencer Colton <me@spencercolton.tech>
2016-07-29 18:50:59 -0400

    Test SPL Autoload for namespaces --likely broken

--- index.php
+++ index.php
@@ -4,6 +4,23 @@ use tech\scolton\tutor\User;
 
 define("PAGE_NAME", "MAIN");
 define("REL", "");
+
+spl_autoload_register(function ($class) {
+    $i = new RecursiveDirectoryIterator(REL, RecursiveDirectoryIterator::SKIP_DOTS);
+    $j = new RecursiveIteratorIterator($i, RecursiveIteratorIterator::SELF_FIRST);
+
+    foreach ($j as $item) {
+        if (strtolower($item->getExtension()) != "php")
+            continue;
+
+        if (strtolower($item->getBasename(".php")) != $class)
+            continue;
+
+        /** @noinspection PhpIncludeInspection */
+        include ($item->getPath());
+    }
+});
+
 session_start();
 if (!isset($_SESSION["id"]) || $_SESSION["id"] == -1)
     header("Location: login");
